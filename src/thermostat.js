931ad03561fcec3c965c8ef63b4d65734d709ec6
James <james.rudrum@hyperisland.se>
2019-02-07 15:02:29 +0000

    Passed test for having a minimum temperature allowed

--- src/thermostat.js
+++ src/thermostat.js
@@ -2,6 +2,7 @@
 
 function Thermostat(){
   this._temperature = 20;
+  this._minimumTemp = 10;
 
 };
 
@@ -14,5 +15,9 @@ Thermostat.prototype.up = function(temp){
 };
 
 Thermostat.prototype.down = function(temp){
+  if(this._temperature - temp < 10){
+    this._temperature = 10;
+  } else{
   this._temperature -= temp;
+  }
 };
