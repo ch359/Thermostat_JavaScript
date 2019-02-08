a8e8b8da39223c50f91c664a59bd77beafdbdf21
Colin Hulme <17675453+ch359@users.noreply.github.com>
2019-02-08 14:48:35 +0000

    Link buttons to objects

--- src/interface.js
+++ src/interface.js
@@ -1,11 +1,48 @@
 'use strict';
+let thermostat = new Thermostat();
 
 $(document).ready(function() {
-    let thermostat = new Thermostat();
-    $('#temperature').text(thermostat.temperature());
+    updateTemperature();
 
     $('#up').on('click', function() {
         thermostat.up();
-        $('#temperature').text(thermostat.temperature());
-    })
+        updateTemperature();
+    });
+
+    $('#down').on('click', function() {
+        thermostat.down();
+        updateTemperature();
+    });
+
+    $('#reset').on('click', function() {
+        thermostat.reset();
+        updateTemperature();
+    });
+
+    $('#powersaving-on').on('click', function() {
+        thermostat.powerSavingOn();
+        updatePowerSaving();
+    });
+
+    $('#powersaving-off').on('click', function() {
+        thermostat.powerSavingOff();
+        updatePowerSaving();
+    });
+
 });
+
+function updateTemperature() {
+    $('#temperature').text(thermostat.temperature());
+}
+
+function updatePowerSaving() {
+    $('#power-saving-status').text(powerSavingStatus())
+}
+
+function powerSavingStatus() {
+    if(thermostat._isPowerSaving) {
+        return 'on'
+    } else {
+        return 'off'
+    }
+}
